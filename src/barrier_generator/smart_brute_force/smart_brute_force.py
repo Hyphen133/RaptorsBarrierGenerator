d5ada167f7fabdc3ea3e0a5b22a8a915b198d3c4
Hyphen133 <hyphen133@gmail.com>
2020-05-24 13:33:07 +0200

    Smart brute force working

--- src/barrier_generator/smart_brute_force/smart_brute_force.py
+++ src/barrier_generator/smart_brute_force/smart_brute_force.py
@@ -1,6 +1,8 @@
 from collections import deque
 
 import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
 
 class SmartBruteForce:
     NOT_CHECKED = 255
@@ -12,44 +14,45 @@ class SmartBruteForce:
         super().__init__()
         self.robot_config = robot_config
 
-    # def extract_unpassable_regions(self, boundary_map):
-    #     pass
+    def thicken_boundaries(self, img):
+        line_threshold = self.robot_config.get_diameter()
+        WHITE_PIXEL = 255
+        out_img = WHITE_PIXEL - img
+        array = self.create_circle_array(line_threshold // 2, line_threshold // 2, line_threshold + 1, line_threshold // 2)
+        out_img = signal.convolve2d(out_img, array, mode='same')
+        out_img = (1 - np.array(out_img > 0, dtype=int)) * WHITE_PIXEL
+        return out_img
 
     def generate_regions(self, map_image):
-        positions_to_be_checked = deque([self.robot_config.get_starting_position()])
-        position_map = np.ones_like(np.array(map_image))*SmartBruteForce.NOT_CHECKED
+        blocked_regions = []
+        map = np.copy(map_image)
 
-        MAP_HEIGHT, MAP_WIDTH = map_image.shape
+        passable_region_map = self.generate_region(map, self.robot_config.get_starting_position())
+        map = self.merge_region_to_map(map, passable_region_map)
+        passable_region = Region(passable_region_map)
 
-        while len(positions_to_be_checked) > 0:
-            current_x,current_y = positions_to_be_checked.popleft()
+        try:
+            while True:
+                next_region_starting_position = self.find_next_region_position(map)
+                region_map = self.generate_region(map, next_region_starting_position)
+                map = self.merge_region_to_map(map, region_map)
+                blocked_regions.append(Region(region_map))
+        except ValueError:
+            return passable_region, blocked_regions
 
-            if map_image[current_x, current_y] == SmartBruteForce.NOT_CHECKED:
-                # if map_image[current_x, current_y] == SmartBruteForce.BOUNDARY:
-                #     position_map[current_x, current_y] = SmartBruteForce.BLOCKED
+    def merge_region_to_map(self, map, region):
+        return map * np.array(region != SmartBruteForce.PASSABLE, dtype=int)
 
-                if self.is_robot_position_possible(map_image, current_x,current_y):
-                    position_map[current_x,current_y] = SmartBruteForce.PASSABLE
+    def find_next_region_position(self, map_image):
+        next_region_starting_position = np.argwhere(map_image == SmartBruteForce.NOT_CHECKED)
+        try:
+            return next_region_starting_position[0]
+        except Exception:
+            raise ValueError("No more UNCHECKED regions found!!")
 
-                    if current_x-1 >= 0:
-                        pos = (current_x-1,current_y)
-                        self.add_position(position_map, pos, positions_to_be_checked)
-                    if current_x+1 < MAP_HEIGHT:
-                        pos = (current_x + 1, current_y)
-                        self.add_position(position_map, pos, positions_to_be_checked)
-                    if current_y-1 >= 0:
-                        pos = (current_x, current_y-1)
-                        self.add_position(position_map, pos, positions_to_be_checked)
-                    if current_y+1 < MAP_WIDTH:
-                        pos = (current_x, current_y+1)
-                        self.add_position(position_map, pos, positions_to_be_checked)
-                else:
-                    position_map[current_x,current_y] = SmartBruteForce.BLOCKED
 
-        return position_map
-
-    def generate_regions_after_boardening(self, map_image):
-        positions_to_be_checked = deque([self.robot_config.get_starting_position()])
+    def generate_region(self, map_image, region_starting_position):
+        positions_to_be_checked = deque([region_starting_position])
         position_map = np.ones_like(np.array(map_image))*SmartBruteForce.NOT_CHECKED
 
         MAP_HEIGHT, MAP_WIDTH = map_image.shape
@@ -81,26 +84,6 @@ class SmartBruteForce:
         if position_map[pos[0], pos[1]] == SmartBruteForce.NOT_CHECKED and pos not in positions_to_be_checked:
             positions_to_be_checked.append(pos)
 
-    def is_robot_position_possible(self, map_image, current_x, current_y):
-        robot_radius = self.robot_config.get_diameter() // 2
-        MAP_HEIGHT,MAP_WIDTH = map_image.shape
-
-        slice_x1 = max(current_x-robot_radius,0)
-        slice_y1 = min(current_x+robot_radius,MAP_HEIGHT-1)+1
-        slice_x2 = max(current_y-robot_radius,0)
-        slice_y2 = min(current_y+robot_radius,MAP_WIDTH-1)+1
-        sliced_map = map_image[slice_x1:slice_y1 ,slice_x2:slice_y2]
-
-        found_boundary = self.found_boundary_in_circle(robot_radius, sliced_map)
-
-        return np.min(sliced_map.shape)>0 and found_boundary
-        # return np.min(sliced_map.shape)>0 and np.min(sliced_map) >= SmartBruteForce.PASSABLE
-
-    def found_boundary_in_circle(self, robot_radius, sliced_map):
-        circle_array = self.create_circle_array(robot_radius, robot_radius, 2 * robot_radius + 1, robot_radius)
-        found_boundary = np.min(sliced_map * (255 * (1 - circle_array) + 1) > SmartBruteForce.BOUNDARY)
-        return found_boundary
-
     def create_circle_array(self, center_x, center_y, square_side, radius, value=1):
         y, x = np.ogrid[-center_x:square_side - center_x, -center_y:square_side - center_y]
         mask = x * x + y * y <= radius * radius
@@ -108,3 +91,21 @@ class SmartBruteForce:
         array[mask] = value
         return array
 
+class Region():
+    def __init__(self, region_map) -> None:
+        super().__init__()
+        self.region_map = region_map
+
+    def get_boundary(self):
+        return (1-np.array(self.region_map==SmartBruteForce.BLOCKED,dtype=int)*255)
+
+    def get_area(self):
+        return (1 - np.array(self.region_map == SmartBruteForce.PASSABLE, dtype=int)*255)
+
+    def show_area(self):
+        plt.imshow(self.get_area(), cmap='gray')
+        plt.show()
+
+    def show_boundary(self):
+        plt.imshow(self.get_boundary(), cmap='gray')
+        plt.show()
--- tests/smart_brute_force_tests.py
+++ tests/smart_brute_force_tests.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SmartBruteForceTests(TestBase):
-    def test_generate_additional_lines(self):
+    def test_generate_regions(self):
         # given
         img = np.array(Image.open("../test_resources/poly2.bmp").convert('L'))
         line_threshold = 40
@@ -18,61 +18,21 @@ class SmartBruteForceTests(TestBase):
         barrier_generator = SmartBruteForce(robot_config)
 
         #tmp
+        thickened_boundary_image = barrier_generator.thicken_boundaries(img)
 
-        img2 = self.widthen_boundaries(barrier_generator, img, line_threshold)
-
-        plt.imshow(img2, cmap='gray')
+        plt.imshow(thickened_boundary_image, cmap='gray')
         plt.title("With widthen boundaries")
         plt.show()
 
         # when
-        image = barrier_generator.generate_regions_after_boardening(img2)
-        plt.imshow(image)
-        plt.title("Extracted region")
-        plt.show()
+        passable_region, impassable_regions = barrier_generator.generate_regions(thickened_boundary_image)
 
-        img3 = img2 * np.array(image != SmartBruteForce.PASSABLE, dtype=int)
 
-        plt.imshow(img3)
-        plt.title("Left image")
-        plt.show()
+        plt.title("Passable region")
+        passable_region.show_boundary()
+        for i,region in enumerate(impassable_regions):
+            plt.title("Impassable region " + str(i))
+            region.show_boundary()
 
-        #-------------------------------------
 
-        next_region_starting_position = np.argwhere(img3 == SmartBruteForce.NOT_CHECKED)[0]
-        barrier_generator.robot_config = RobotConfig(line_threshold,next_region_starting_position)
-
-        image2 = barrier_generator.generate_regions_after_boardening(img2)
-        plt.imshow(image2)
-        plt.title("Extracted region2")
-        plt.show()
-
-        img4 = img3 * np.array(image2 != SmartBruteForce.PASSABLE, dtype=int)
-
-        plt.imshow(img4)
-        plt.title("Left image2")
-        plt.show()
-
-        # then
-        # passable_img = np.array(image == SmartBruteForce.PASSABLE, dtype=int) * 255
-        # plt.imshow(passable_img, cmap='gray')
-        # plt.show()
-        #
-        # plt.imshow(np.array((255-passable_img)*img2 > 0, dtype=int), cmap='gray')
-        # plt.show()
-
-        # self.assertEqual(img.shape[:2], np.array(image).shape[:2])
-
-    def widthen_boundaries(self, barrier_generator, img, line_threshold):
-        img2 = 255 - img
-        a, b = line_threshold // 2, line_threshold // 2
-        n = line_threshold + 1
-        r = line_threshold // 2
-        array = barrier_generator.create_circle_array(a, b, n, r)
-        # scharr = np.array([[1, 1, 1],
-        #                    [1, 1, 1],
-        #                    [1, 1, 1]])  # Gx + j*Gy
-        img2 = signal.convolve2d(img2, array, mode='same')
-        img2 = (1 - np.array(img2 > 0, dtype=int)) * 255
-        return img2
 
